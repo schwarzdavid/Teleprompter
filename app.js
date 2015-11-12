f0b7fec63b3bc66667964a0e7e270f48d8943c0d
David Schwarz <schwarz.david10@gmail.com>
2015-11-12 16:32:29 +0100

    Yay

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ var express = require('express'),
 	sio = require('socket.io'),
 	fs = require('fs'),
 	session = require('express-session'),
+	room = require('./room.js'),
 	app = express(),
-	http;
+	http, room, manager;
 
 app.set('views', 'web/views');
 app.set('view engine', 'ejs');
+
+app.use(session({
+	secret: 't3l3pr0mpt3r 1s b35cht3',
+	resave: false,
+	saveUninitialized: true
+}));
 
 app.get('/page/:page', function(req, res){
 	var page = req.param.page.replace('html','ejs');
@@ -16,10 +23,6 @@ app.get('/page/:page', function(req, res){
 		}
 		res.render('./web/views/pages/'+page);
 	});
-});
-
-app.get('/host', function(req, res, next){
-	
 });
 
 app.get('/', function(req, res){
@@ -39,4 +42,54 @@ http = app.listen(80, function(err){
 	require('./core/sockets.js')(http);
 });
 
+manager = (function(){
+	var Room = function(con){
+		this.uid = Math.floor(Math.random() * 90000 + 10000);
+
+		this.addClient = function(con){
+
+		};
+
+		this.removeClient = function(con){
+
+		};
+
+		this.editClient = function(x, y){
+
+		};
+	};
+	
+	var rooms = [];
+
+	return {
+		createRoom: function(con){
+			var r = new Room(con);
+			rooms.push(r);
+			
+			return r.uid;
+		},
+		
+		deleteRoom: function(){
+			
+		},
+		
+		findRoom: function(){
+			
+		}
+	}
+}());
+
 sio = sio(http);
+sio.on('connection', function(socket){
+	socket.room = null;
+	
+	socket.on('host', function(data){
+		if(!socket.room){
+			socket.room = new room(socket);
+		}
+	});
+	
+	socket.on('join', function(data){
+		socket.room = 
+	});
+});
