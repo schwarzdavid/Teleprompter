445f59e0253864e7ecfdd855ab8ed6447f008391
David Schwarz <schwarz.david10@gmail.com>
2015-12-18 14:05:19 +0100

    Openshift Shizzl

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express'),
-	sio = require('socket.io'),
 	fs = require('fs'),
 	session = require('express-session'),
 	manager = require('./core/manager.js'),
+	http = require('http'),
 	app = express(),
-	http;
+	server = http.Server(app),
+	sio = require('socket.io')(server);
 
 app.set('views', 'web/views');
 app.set('view engine', 'ejs');
@@ -32,14 +33,13 @@ app.get('*', function(req, res){
 	res.render('template.ejs');
 });
 
-http = app.listen(process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', function(err){
+server.listen(process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', function(err){
 	if(err){
 		throw new Error(err);
 	}
 	console.log('Webserver gestartet...');
 });
 
-sio = sio(http);
 sio.on('connection', function(socket){
 	socket.room = null;
 	
